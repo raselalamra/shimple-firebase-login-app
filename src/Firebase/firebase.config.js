ee1b257d541e17dcdd67b655d113a4827dc917fb
raselalamjubayed <raselalam838@gmil.com>
2021-10-11 03:01:31 +0600

    all code added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9xcakrwh8xkj0fqDcPzzEO-Usqpw6zxw",
+    authDomain: "shimple-firebase-login.firebaseapp.com",
+    projectId: "shimple-firebase-login",
+    storageBucket: "shimple-firebase-login.appspot.com",
+    messagingSenderId: "877989055408",
+    appId: "1:877989055408:web:d5379640132fe4addba475"
+  };
+
+  export default firebaseConfig;
